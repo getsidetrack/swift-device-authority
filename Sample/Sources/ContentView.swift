6ce4af39c199f5fe7b3dede855eafc35384fc82c
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2023-01-05 23:16:26 +0000

    Create boilerplate Sample project

---
+++ Sample/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeviceAuthority
+//
+//  Created by James Sherlock on 05/01/2023.
+//
+
+import DeviceAuthority
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Sample/Sources/DeviceAuthorityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceAuthorityApp.swift
+//  DeviceAuthority
+//
+//  Created by James Sherlock on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeviceAuthorityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
